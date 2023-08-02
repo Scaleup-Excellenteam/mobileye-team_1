3c942710778a9bb5fe158ea74a392e130ef32ce8
ranelbe <ranelbe@edu.hac.ac.il>
2023-08-03 00:02:25 +0300

    high pass filter + nms

--- part_1.py
+++ part_1.py
@@ -5,76 +5,67 @@ from pathlib import Path
 
 import cv2
 import numpy as np
-import scipy.ndimage as ndimage
-from scipy import signal as sg
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy.ndimage import maximum_filter
 from scipy.signal import convolve2d
 
-# if you wanna iterate over multiple files and json, the default source folder name is this.
 DEFAULT_BASE_DIR: str = './test'
-
-# The label we wanna look for in the polygons json file
 TFL_LABEL = ['traffic light']
-
 POLYGON_OBJECT = Dict[str, Union[str, List[int]]]
 RED_X_COORDINATES = List[int]
 RED_Y_COORDINATES = List[int]
 GREEN_X_COORDINATES = List[int]
 GREEN_Y_COORDINATES = List[int]
 
-
-def convolve(img):
-    kernel = np.array([[-1, -1, -1],
-                       [-1, 8, -1],
-                       [-1, -1, -1]])
-
-    return sg.convolve(img, kernel, "same")
-
-
-def find_tfl_lights(c_image: np.ndarray,
-                    **kwargs) -> Tuple:
+def high_pass_filter(image: np.ndarray, size: int = 3) -> np.ndarray:
     """
-    Detect candidates for TFL lights. Use c_image, kwargs and your imagination to implement.
+    Filter the image using a high pass filter.
+    :param image: The image itself as np.uint8, shape of (H, W, 3).
+    :param size: The size of the filter.
+    :return: The filtered image.
+    """
+    kernel = np.ones((size, size)) / -1
+    kernel[1, 1] = 8
+    return convolve2d(image, kernel, mode='same')
 
+
+def non_max_suppression(image: np.ndarray, size: int = 5) -> np.ndarray:
+    """
+    Suppress non-maximum values in the image.
+    :param image: The image itself as np.uint8, shape of (H, W, 3).
+    :param size: The size of the filter.
+    """
+    max_filter = maximum_filter(image, size=size)
+    suppressed_image = np.zeros(image.shape)
+    is_local_maximum = (image == max_filter)
+    suppressed_image[is_local_maximum] = image[is_local_maximum]
+    return suppressed_image
+
+
+def find_tfl_lights(c_image: np.ndarray) -> \
+        Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
+    """
+    Detect candidates for TFL lights.
     :param c_image: The image itself as np.uint8, shape of (H, W, 3).
-    :param kwargs: Whatever config you want to pass in here.
     :return: 4-tuple of x_red, y_red, x_green, y_green.
     """
-
-    # Apply max filter to remove false positive detections
-    max_filtered_image = ndimage.maximum_filter(c_image, 2)
-    # cv2.imshow("max", max_filtered_image)
-
-    # Add blur to the image to reduce noise
-    c_image = cv2.GaussianBlur(max_filtered_image, (3, 3), 0.5)
-
     # Convert the input RGB image to HSV color space
     hsv_image = cv2.cvtColor(c_image, cv2.COLOR_RGB2HSV)
-    # cv2.imshow("hsv", hsv_image)
 
-    # Get the V channel from the HSV image
+    # high pass filter and non-maximum suppression on the v channel
     v_channel = hsv_image[:, :, 2]
-    # cv2.imshow("channel", v_channel)
-
-    # Apply convolution to enhance edges and intensity changes using the Laplacian kernel
-    kernel = np.array([[-1, -1, -1],
-                       [-1, 8, -1],
-                       [-1, -1, -1]])
-    convolved_v_channel = convolve2d(v_channel, kernel, mode='same')
-
-    # Threshold the convolved V channel to create a binary mask
-    threshold = 100
-    mask = (convolved_v_channel > threshold).astype(np.uint8)
-
+    convolved_v_channel = non_max_suppression(
+        high_pass_filter(v_channel)
+    )
     # Define the range of red and green colors in HSV space
     lower_red = np.array([0, 100, 100])
-    upper_red = np.array([30, 255, 255])
+    upper_red = np.array([15, 255, 255])
     lower_green = np.array([40, 100, 100])
-    upper_green = np.array([90, 255, 255])
+    upper_green = np.array([100, 255, 255])
 
-    # Create masks for red and green regions
+    # Create masks for red and green regions and for the high-pass filtered image
+    mask = (convolved_v_channel > 150).astype(np.uint8)
     mask_red = cv2.inRange(hsv_image, lower_red, upper_red) & mask
     mask_green = cv2.inRange(hsv_image, lower_green, upper_green) & mask
 
@@ -82,6 +73,7 @@ def find_tfl_lights(c_image: np.ndarray,
     red_coords = np.argwhere(mask_red)
     green_coords = np.argwhere(mask_green)
 
+    # Get the x and y coordinates of red and green regions
     x_red, y_red = red_coords[:, 1], red_coords[:, 0]
     x_green, y_green = green_coords[:, 1], green_coords[:, 0]
 
